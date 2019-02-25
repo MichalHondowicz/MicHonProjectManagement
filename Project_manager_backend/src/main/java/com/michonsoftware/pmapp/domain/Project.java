967b4d2d017c749b1d67e48b04fc7e573e381614
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-02-25 21:59:59 +0100

    Service, Controller initiated. PostMapping: createProject method added

--- Project_manager_backend/src/main/java/com/michonsoftware/pmapp/domain/Project.java
+++ Project_manager_backend/src/main/java/com/michonsoftware/pmapp/domain/Project.java
@@ -3,6 +3,8 @@ package com.michonsoftware.pmapp.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
@@ -12,9 +14,18 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Project name is necessary")
     private String projectName;
+
+    @NotBlank(message = "Project identifier is necessary")
+    @Size(min = 4, max = 5, message = "Project identifier to be 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project description is necessary")
     private String projectDescription;
+
     private LocalDate startDate;
     private LocalDate endDate;
     private LocalDate createdOn;
