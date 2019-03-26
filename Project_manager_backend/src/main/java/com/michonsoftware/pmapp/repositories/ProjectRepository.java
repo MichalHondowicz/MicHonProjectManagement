07725d961fa904a0436526692e1f17443244cab7
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-03-26 21:17:37 +0100

    Errors mapping introduced to Actions

--- Project_manager_backend/src/main/java/com/michonsoftware/pmapp/repositories/ProjectRepository.java
+++ Project_manager_backend/src/main/java/com/michonsoftware/pmapp/repositories/ProjectRepository.java
@@ -1,6 +1,5 @@
 package com.michonsoftware.pmapp.repositories;
 
-
 import com.michonsoftware.pmapp.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
